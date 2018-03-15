070bd0eeb7c588a22b7ec9de7991910eac05e44d
Imam Asari <tahzan.la10@gmail.com>
2018-03-16 00:14:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# IonicOfflineMapOpenStreetMap
+
+Ionic Offline Map (Peta Offline Ionic) dg Open Street Map & Leaflet
+
+[Tutorial](http://blog.babanggi.com/2018/03/ionic-offline-map-peta-offline-ionic-dg.html)
+
+by: [https://blog.babanggi.com](https://blog.babanggi.com)
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import "leaflet";
+import "leaflet-tilelayer-mbtiles-ts";
+
+declare var L: any;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
+    map: L.Map;
+    center: L.PointTuple;
+    
+    constructor(public navCtrl: NavController) { }
 
-  constructor(public navCtrl: NavController) {
+    ionViewDidEnter() {  
+        this.prepareMap();
+    }
 
-  }
+    prepareMap() {
+        this.map = L.map('map', {
+            center: [-6.177635, 106.826439],
+            zoom: 7,
+            minZoom: 7,
+            maxZoom: 10,
+            attributionControl: true,
+            zoomControl:true
+        });
 
+        // Database menggunakan file png di asset (offline)
+        L.tileLayer('./assets/imgs/maps/{z}/{x}/{y}.jpg').addTo(this.map);
+
+        // Database Online, dg query langsung ke website open street map
+        // L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+        //    .addTo(this.map); 
+    }
 }
